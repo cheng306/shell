3d48629ed7a70a1fe19c9fcebbcd7d9d64c46cb4
Johnny Cheng <johnnycheng0306@gmail.com>
2022-03-10 14:46:31 -0500

    1-10

--- script.sh
+++ script.sh
@@ -14,45 +14,51 @@
 #    esac
 # done
 
-x=wha
-case $x in
-    0) echo 0 ;;
-    1) echo 1 ;;
-    wha) echo wha ;;
-esac
+## case ## 
+# x=wha
+# case $x in
+#     0) echo 0 ;;
+#     1) echo 1 ;;
+#     wha) echo wha ;;
+# esac
 
 # set -x
 
-X="12 a"
-if [ "$X" -lt "0" ]
+
+## if case ##
+X="12"
+if [ $X -lt 0 ]
 then
   echo "X is less than zero"
 fi
-if [ "$X" -gt "0" ]; then
+if [ $X -gt 0 ]; then
   echo "X is more than zero"
 fi
 
-[ "$X" -le "0" ] && \
-      echo "X is less than or equal to  zero"
-[ "$X" -ge "0" ] && \
-      echo "X is more than or equal to zero"
-[ "$X" = "0" ] && \
-      echo "X is the string or number \"0\""
-[ "$X" = "hello" ] && \
-      echo "X matches the string \"hello\""
-[ "$X" != "hello" ] && \
-      echo "X is not the string \"hello\""
-[ -n "$X" ] && \
-      echo "X is of nonzero length" || echo "X is zero"
-[ -f "$X" ] && \
-      echo "X is the path of a real file" || \
-      echo "No such file: $X"
-[ -x "$X" ] && \
-      echo "X is the path of an executable file"
-[ "$X" -nt "/etc/passwd" ] && \
-      echo "X is a file which is newer than /etc/passwd"
+# [ "$X" -le "0" ] && \
+#       echo "X is less than or equal to  zero"
+# [ "$X" -ge "0" ] && \
+#       echo "X is more than or equal to zero"
+# [ "$X" = "0" ] && \
+#       echo "X is the string or number \"0\""
+# [ "$X" = "hello" ] && \
+#       echo "X matches the string \"hello\""
+# [ "$X" != "hello" ] && \
+#       echo "X is not the string \"hello\""
+# [ -n "$X" ] && \
+#       echo "X is of nonzero length" || echo "X is zero"
+# [ -f "$X" ] && \
+#       echo "X is the path of a real file" || \
+#       echo "No such file: $X"
+# [ -x "$X" ] && \
+#       echo "X is the path of an executable file"
+# [ "$X" -nt "/etc/passwd" ] && \
+#       echo "X is a file which is newer than /etc/passwd"
 
-export y="export message"
-echo "main:" $main
+# export y="export message"
+# echo "main:" $main
 
-echo "123-456-789"
+# echo "123-456-789"
+
+b=12
+c=13
